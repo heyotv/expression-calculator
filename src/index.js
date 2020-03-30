7fd961d1ac751708c80666fe7cdfcf8b7c8119f3
heyotv <hey.tumilovich@gmail.com>
2020-03-30 13:31:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,86 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const operators = {
+        '+': (x, y) => (stack[stack.length - 1] === '-') ? x - y : x + y,
+        '-': (x, y) => (stack[stack.length - 1] === '-') ? x + y : x - y,
+        '*': (x, y) => x * y,
+        '/': (x, y) => {
+            if (y === 0) {
+                throw 'TypeError: Division by zero.';
+            } else {
+                return x / y;
+            }
+        }
+    };
+
+    const priority = {
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2
+    }
+
+    let bracketsCount = 0;
+    let arr = [];
+    let stack = [];
+    let result = [];
+
+    expr = expr.split(')').join(' ) ').split('(').join(' ( ');
+
+    if (expr.indexOf(' ') != -1) {
+        arr = expr.trim().split(' ');
+    } else {
+        arr = expr.trim().split('');
+    }
+    arr = arr.filter((item) => {
+        return item != '';
+    });
+
+    arr.forEach((item) => {
+        if (item === '(') {
+            bracketsCount++;
+        }
+        if (item === ')') {
+            bracketsCount--;
+        }
+    });
+
+    if (bracketsCount != 0) {
+        throw 'ExpressionError: Brackets must be paired';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!isNaN(arr[i])) {
+            result.push(+arr[i]);
+        } else {
+            if (arr[i] in operators) {
+                if (priority[arr[i]] <= priority[stack[stack.length - 1]]) {
+                    calculate();
+                }
+            }
+            if (arr[i] === ')') {
+                while (stack[stack.length - 1] != '(') {
+                    calculate();
+                }
+                stack.pop();
+                continue;
+            }
+            stack.push(arr[i]);
+        }
+    }
+
+    while (stack.length != 0) {
+        calculate();
+    }
+
+    return result.pop();
+
+    function calculate() {
+        let [y, x] = [result.pop(), result.pop()];
+        result.push(operators[stack.pop()](x, y));
+    }
+
 }
 
 module.exports = {
